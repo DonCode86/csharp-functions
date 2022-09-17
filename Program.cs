9608f4a0cdd1a5ddc417fe9cdf64b8edffa14f21
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-17 14:46:50 +0200

    funciont 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] arrayInput = { 2, 6, 7, 5, 3, 9 };
+
+void StampaArray(int[] array, char openP = '[', char closeP = ']')
+{
+    Console.Write(openP);
+
+    int i;
+
+    for (i = 0; i < array.Length -1; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(",");
+    }
+
+    Console.Write(array[i]);
+
+    Console.Write(closeP);
+}
+Console.WriteLine("La lista dei numeri è: ");
+StampaArray(arrayInput);
+
+int Quadrato(int numero)
+{
+    Console.WriteLine(numero * numero);
+    return numero;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = (int)Math.Pow(array[i], 2);
+    }
+    return result;
+}
+Console.WriteLine("I numeri elevati al quadrato sono: ");
+StampaArray(ElevaArrayAlQuadrato(arrayInput));
+
+int sommaElementiArray(int[] array)
+{
+    int somma = 0;
+
+    foreach (int i in array)
+    {
+        somma += i;
+    }
+
+    return somma;
+}
+Console.WriteLine("La somma dei numeri dell'array è: ");
+Console.WriteLine(sommaElementiArray(arrayInput));
